eb6b0b00af89a35b995cf5e1f480cfb046f6588c
mv45-eng <mateovallejo45@gmail.com>
2026-04-14 07:46:19 -0500

    Primer ejercicio del examen

---
+++ ejercicio1 combate.py
@@ -0,0 +1,12 @@
+def calcular dano(ataque, defensa):
+  return ataque - defensa
+
+print(calcular dano(10, 5))
+
+aplicar dano(hp actual, dano):
+  resultado = hp actual - dano
+    if resultado > 0:
+      return hp actual - dano
+    else:
+      return 0
+print(resultado)
