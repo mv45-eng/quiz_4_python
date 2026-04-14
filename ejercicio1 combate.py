79c63ab48106f8a52f9569a14a4f3250d30bf852
mv45-eng <mateovallejo45@gmail.com>
2026-04-14 08:03:23 -0500

    Primer ejercicio del examen

--- ejercicio1 combate.py
+++ ejercicio1 combate.py
@@ -1,12 +1,30 @@
-def calcular dano(ataque, defensa):
+hp_heroe = 100
+hp_enemigo = 50.
+
+estado_inicial = hp_heroe == 100 and hp_enemigo == 50.
+
+
+
+def calcular_dano(ataque, defensa):
   return ataque - defensa
 
-print(calcular dano(10, 5))
 
-aplicar dano(hp actual, dano):
-  resultado = hp actual - dano
-    if resultado > 0:
-      return hp actual - dano
-    else:
+def aplicar_dano(hp_actual, dano):
+  resultado = hp_actual - dano
+  if resultado > 0:
+      return hp_actual - dano
+  else:
       return 0
-print(resultado)
+
+
+def realizar_ataque(atacante, defensor, dano):
+  return(f"{atacante} ataca a {defensor} por {dano} de daño!")
+
+print(f"----Estado Inicial----")
+print(f"HP Heroe: {hp_heroe}")
+print(f"HP Enemigo: {hp_enemigo}")
+print("-------------------------")
+print(calcular_dano(25, 10))
+print(realizar_ataque("Mateo", "Slime", 15))
+print(aplicar_dano(hp_enemigo, 15))
+print(f"Estado del enemigo: {hp_enemigo-15}")
